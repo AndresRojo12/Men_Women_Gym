9c988c39dd0130fc0f71ba2e73357dff0f62bcbe
Andres <andresrojo20013@gmail.com>
2026-02-04 13:05:02 -0500

    se importa configModule en el modulo raiz appModule #4

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExercisesModule } from './exercises/exercises.module';
 
 @Module({
-  imports: [ExercisesModule],
+  imports: [ConfigModule.forRoot(), ExercisesModule],
   controllers: [AppController],
   providers: [AppService],
 })
