6858328095a681b64d7035865b687e6a8d108a5e
Andres <andresrojo20013@gmail.com>
2026-02-05 13:42:41 -0500

    se crea entidad Exercise #5

---
+++ src/exercises/entities/Exercise.dto.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Exercise {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+    
+    @Column()
+    level: string;
+}
