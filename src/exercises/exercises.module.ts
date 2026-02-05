9380412c96710c8aa92231d61710f26e6e977eee
Andres <andresrojo20013@gmail.com>
2026-02-05 13:57:31 -0500

    se importa entidad en el modulo correspondiente exercise #5

--- src/exercises/exercises.module.ts
+++ src/exercises/exercises.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ExercisesController } from './controller/exercises.controller';
 import { ExercisesService } from './services/exercises.service';
+import { Exercise } from './entities/Exercise.entity';
 
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forFeature([Exercise])],
     controllers: [ExercisesController],
     providers: [ExercisesService],
 })
