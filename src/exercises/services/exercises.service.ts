4ad9e0db2976cc293c4a23de5114b105dfd22892
Andres <andresrojo20013@gmail.com>
2026-02-05 14:24:29 -0500

    se crea servicio exercise #5

--- src/exercises/services/exercises.service.ts
+++ src/exercises/services/exercises.service.ts
@@ -1,10 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Exercise } from '../entities/Exercise.entity';
+import { Not, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateExerciseDto } from '../dtos/Exercise.dto';
 
 @Injectable()
 export class ExercisesService {
 
+  constructor(
+    @InjectRepository(Exercise)
+    private exercisesRepository: Repository<Exercise>,
+  ) {}
+
+  findAll(): Promise<Exercise[]> {
+    return this.exercisesRepository.find();
+  }
+
   create(data: CreateExerciseDto) {
-    console.log('Exercise created:', data);
+    const newExercise = this.exercisesRepository.create(data);
+    return this.exercisesRepository.save(newExercise);
+  }
+
+  async findOne(id: number) {
+    const exercise = await this.exercisesRepository.findOneBy({ id });
+    if(!exercise){
+      throw new NotFoundException(`Exercise with id ${id} not found`);  
+    }
+    return exercise;
+  } 
+
+  async update(id: number, changes: Partial<CreateExerciseDto>) {
+    const exercise = await this.exercisesRepository.findOneBy({ id });
+    this.exercisesRepository.merge(exercise, changes);
+    return this.exercisesRepository.save(exercise);
+  }
+
+  async remove(id: number) {
+    return this.exercisesRepository.delete(id);
   }
 }
