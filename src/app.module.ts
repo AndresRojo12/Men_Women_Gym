3e88a08f5b9b0a8c60333863612a02e2c90c7895
Andres <andresrojo20013@gmail.com>
2026-02-05 12:26:16 -0500

    se crea modulo database y se importa en el modulo raiz #5

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExercisesModule } from './exercises/exercises.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: `.env`,
     isGlobal: true,
-  }), ExercisesModule],
+  }), ExercisesModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
