404b3d40e24e674649e0a2dc1d2a144174eaaafb
Andres <andresrojo20013@gmail.com>
2026-02-05 13:09:33 -0500

    se define .env para variables de entorno #5

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -5,8 +5,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     imports: [TypeOrmModule.forRoot({
         type: 'postgres',
         host: process.env.DB_HOST,
-        port: 5432,
-        username: process.env.DB_USERNAME,
+        port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+        username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
