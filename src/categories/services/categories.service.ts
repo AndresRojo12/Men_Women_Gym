3b78c792717871687ef67c733694eebac587629a
Andres <andresrojo20013@gmail.com>
2026-02-12 14:44:59 -0500

    se crea servicio category #9

--- src/categories/services/categories.service.ts
+++ src/categories/services/categories.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException, NotFoundException } from '@nestjs/common';
+import { Category } from '../entities/Category.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateCategoryDto, UpdateCategoryDto } from '../dtos/Category.dto';
+
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+    constructor(
+        @InjectRepository(Category)
+        private categoriesRepository: Repository<Category>,
+    ){}
+
+    findAll(): Promise<Category[]> {
+        return this.categoriesRepository.find();
+    }
+
+    create(data: CreateCategoryDto) {
+        /*const vUnique = this.categoriesRepository.findOne({ where: {name: data.name}, });
+        if(!vUnique){
+          throw new NotFoundException(`Category with name ${data.name} created`);  
+        }*/
+        const newCategory = this.categoriesRepository.create(data);
+        if(newCategory){
+          throw new ConflictException(`Category with name ${data.name} already exists`);
+        }
+        return this.categoriesRepository.save(newCategory);
+    }
+
+    async findOne(id: number) {
+        const category = await this.categoriesRepository.findOneBy({ id });
+        if(!category){
+          throw new NotFoundException(`Category with id ${id} not found`);  
+        }
+        return category;
+    } 
+
+    async update(id: number, changes: UpdateCategoryDto) {
+        const category = await this.categoriesRepository.findOneBy({ id });
+        if(!category){
+          throw new NotFoundException(`Category with id ${id} not found`);  
+        }
+        this.categoriesRepository.merge(category, changes);
+        return this.categoriesRepository.save(category);
+    }
+
+    async remove(id: number) {
+        return this.categoriesRepository.delete(id);
+    }
+
+}
+
+
