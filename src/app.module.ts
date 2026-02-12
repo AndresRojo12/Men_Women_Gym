cf4a9a259ef68d95356c3fc4e3ffc50a5a30f1ba
Andres <andresrojo20013@gmail.com>
2026-02-12 10:53:41 -0500

    se genera modulo de categories #4

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExercisesModule } from './exercises/exercises.module';
 import { DatabaseModule } from './database/database.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: `.env`,
     isGlobal: true,
-  }), ExercisesModule, DatabaseModule],
+  }), ExercisesModule, DatabaseModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
