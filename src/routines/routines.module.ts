c49852ac0f04766e71de91f7dbf1878b1ea06c8d
Andres <andresrojo20013@gmail.com>
2026-02-17 14:18:51 -0500

    se importa entidad en el modulo routines #13

--- src/routines/routines.module.ts
+++ src/routines/routines.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Routine } from './entities/routine.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Routine])],
+})
 export class RoutinesModule {}
