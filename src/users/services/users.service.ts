3f0ccef68c0b7c2e97365bf869b306b6742b7e3e
Andres <andresrojo20013@gmail.com>
2026-02-18 13:41:02 -0500

    se crea servicio de users #18

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException, NotFoundException } from '@nestjs/common';
+import { User } from '../entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto, UpdateUserDto } from '../dtos/User.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
+    ){}
+
+    async findAll(): Promise<User[]> {
+        return await this.usersRepository.find();
+    }
+
+    async create(data: CreateUserDto) {
+        const existingUser = await this.usersRepository.findOne({
+            where: { email: data.email },
+        });
+        if (existingUser) {
+            throw new ConflictException(
+                `User with email ${data.email} already exists`,
+            );
+        }
+        const newUser = this.usersRepository.create(data);
+        return await this.usersRepository.save(newUser);
+    }
+
+    async findOne(id: number) {
+        const user = await this.usersRepository.findOneBy({ id });
+        if (!user) {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
+        return user;
+    }
+
+    async update(id: number, data: UpdateUserDto) {
+        const user = await this.usersRepository.findOneBy({ id });
+        if (!user) {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
+        const updatedUser = this.usersRepository.merge(user, data);
+        return await this.usersRepository.save(updatedUser);
+    }
+
+    async remove(id: number) {
+        const user = await this.usersRepository.findOneBy({ id });
+        if (!user) {
+            throw new NotFoundException(`User with id ${id} not found`);
+        }
+        await this.usersRepository.delete(id);
+        return { message: `User with id ${id} has been removed` };
+    }
+}
