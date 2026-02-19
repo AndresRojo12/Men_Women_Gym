de149350112c4adc3bdffb1475b222109c485c64
Andres <andresrojo20013@gmail.com>
2026-02-19 12:07:11 -0500

    se modifica servicio auth #20

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,7 +1,23 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../../users/services/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<{ access_token: string }> {
+    const user = await this.usersService.findOne(email);
+    if (user?.password !== password) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
 }
