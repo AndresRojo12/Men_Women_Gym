116184c217692d7e850a5e87e3e54dce0bc3dabf
Andres <andresrojo20013@gmail.com>
2026-02-23 11:56:46 -0500

    se implementa el metodo login #20

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -17,5 +17,11 @@ export class AuthService {
     }
     return null;
   }
-    
+  
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.id};
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
 }
