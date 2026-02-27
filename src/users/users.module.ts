6faed8c7b153e6b154a45cdd2cba0fc7af19f62a
Andres <andresrojo20013@gmail.com>
2026-02-27 12:19:35 -0500

    se importa servicio,controladores,entidades #17

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { Customer } from './entities/customer.entity';
 import { UsersService } from './services/users.service';
+import { CustomerService } from './services/customer.service';
 import { UsersController } from './controllers/users.controller';
+import { CustomerController } from './controllers/customer.controller';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
-    providers: [UsersService],
-    controllers: [UsersController],
+    imports: [TypeOrmModule.forFeature([User, Customer])],
+    providers: [UsersService, CustomerService],
+    controllers: [UsersController, CustomerController],
     exports: [UsersService]
 })
 export class UsersModule {}
