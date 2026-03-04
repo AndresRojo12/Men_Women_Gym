6c1e3d0c240ace1714c01e666ef68498b7259a38
Andres <andresrojo20013@gmail.com>
2026-03-04 11:29:26 -0500

    se valida que solo los usuario administradores creen categorias #10

--- src/categories/controllers/categories.controller.ts
+++ src/categories/controllers/categories.controller.ts
@@ -1,36 +1,52 @@
-import { Controller, Get, Post, ParseIntPipe, Delete, Put, Param, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  ParseIntPipe,
+  Delete,
+  Put,
+  Param,
+  Body,
+  UseGuards,
+} from '@nestjs/common';
+import { Roles } from '../../auth/decorators/roles.decorator';
+import { Role } from '../../auth/roles/rol.enum';
+import { RolesGuard } from '../../auth/guards/roles.guard';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
 import { CategoriesService } from '../services/categories.service';
 import { CreateCategoryDto } from '../dtos/Category.dto';
 
 @Controller('categories')
 export class CategoriesController {
-    constructor(private categoriesService: CategoriesService){}
-    
-    @Post() 
-    createCategory(@Body() data: CreateCategoryDto){
-        return this.categoriesService.create(data);
-    }
+  constructor(private categoriesService: CategoriesService) {}
+  
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @Post()
+  createCategory(@Body() data: CreateCategoryDto) {
+    return this.categoriesService.create(data);
+  }
 
-    @Get()
-    getCategories() {
-        return this.categoriesService.findAll();
-    }
+  @Get()
+  getCategories() {
+    return this.categoriesService.findAll();
+  }
 
-    @Get(':id')
-    async findOneCategory(@Param('id', ParseIntPipe) id: number) {
-        return this.categoriesService.findOne(id);
-    }
+  @Get(':id')
+  async findOneCategory(@Param('id', ParseIntPipe) id: number) {
+    return this.categoriesService.findOne(id);
+  }
 
-    @Put(':id')
-    async updateCategory(@Param('id', ParseIntPipe) id: number, @Body() changes: CreateCategoryDto) {
-        return this.categoriesService.update(id, changes);
-    }
+  @Put(':id')
+  async updateCategory(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() changes: CreateCategoryDto,
+  ) {
+    return this.categoriesService.update(id, changes);
+  }
 
-    @Delete(':id')
-    async deleteCategory(@Param('id', ParseIntPipe) id: number) {
-        return this.categoriesService.remove(id);
-    }
-
-
-
+  @Delete(':id')
+  async deleteCategory(@Param('id', ParseIntPipe) id: number) {
+    return this.categoriesService.remove(id);
+  }
 }
