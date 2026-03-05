6b34ec840028daea6528cea1956159ad108a2a32
Andres <andresrojo20013@gmail.com>
2026-03-05 11:32:13 -0500

    se crea la migracion routine_exercise correctamente #26

--- src/routines/routines.module.ts
+++ src/routines/routines.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Routine } from './entities/routine.entity';
-import { RoutineExercise } from './entities/routine_exercise.entity';
 import { Customer } from '../users/entities/customer.entity';
 import { RoutinesService } from './services/routines.service';
 import { RoutinesController } from './controllers/routines.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Routine, RoutineExercise, Customer])],
+  imports: [TypeOrmModule.forFeature([Routine, Customer])],
   providers: [RoutinesService],
   controllers: [RoutinesController],
 })
