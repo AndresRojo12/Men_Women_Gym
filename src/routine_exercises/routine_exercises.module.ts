fa6db53a545fcca21a2b7c8260343aa447d821c2
Andres <andresrojo20013@gmail.com>
2026-03-05 12:03:37 -0500

    se importan entidades al modulo routine_exercise #27

--- src/routine_exercises/routine_exercises.module.ts
+++ src/routine_exercises/routine_exercises.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoutineExercise } from '../routine_exercises/entities/Routine_Exercise.entity';
+import { Routine } from '../routines/entities/routine.entity';
+import { Exercise } from '../exercises/entities/Exercise.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RoutineExercise])],
+  imports: [TypeOrmModule.forFeature([RoutineExercise, Routine, Exercise])],
   controllers: [],
   providers: [],
 })
