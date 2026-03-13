e9ed9683093e269e5ec2febab8bdeb6b1f31195f
Andres <andresrojo20013@gmail.com>
2026-03-13 11:10:20 -0500

    se configura main.ts para carpeta uploads #4

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
-    prefix: '/uploads/',
-  });
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
 
   app.enableCors({
     origin: '*',
