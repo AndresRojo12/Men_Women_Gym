d4a402a877b68c91a16cb228646c9de2f12e5faf
Andres <andresrojo20013@gmail.com>
2026-06-10 13:02:50 -0500

    se ejecuta migración de nutrition_goals #44

--- src/database/migrations/1781113732025-CreateNutritionGoals.ts
+++ src/database/migrations/1781113732025-CreateNutritionGoals.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateNutritionGoals1781113134183 implements MigrationInterface {
-    name = 'CreateNutritionGoals1781113134183'
+export class CreateNutritionGoals1781113732025 implements MigrationInterface {
+    name = 'CreateNutritionGoals1781113732025'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "nutrition_goals" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "description" character varying NOT NULL, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "UQ_00e7178a8b3045aae14e5c15756" UNIQUE ("name"), CONSTRAINT "PK_843d6ec58065c4f34aabc9052a3" PRIMARY KEY ("id"))`);
