9a09ab69746dee90367e13fa10146344a54b598f
Andres <andresrojo20013@gmail.com>
2026-06-25 14:46:56 -0500

    se crea servicio de nueva nutritions_goals #42

--- src/nutritions/services/nutritions.service.ts
+++ src/nutritions/services/nutritions.service.ts
@@ -1,9 +1,16 @@
-import { Injectable, ConflictException, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
 
 import { Nutrition_Goals } from '../entities/Nutrition_Goals.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CreateNutritionGoalDto, UpdateNutritionGoalDto } from '../dtos/Nutrition.dto';
+import {
+  CreateNutritionGoalDto,
+  UpdateNutritionGoalDto,
+} from '../dtos/Nutrition_Goals.dto';
 
 @Injectable()
 export class NutritionsService {
@@ -14,5 +21,18 @@ export class NutritionsService {
   async findAll(): Promise<Nutrition_Goals[]> {
     const nutritionGoals = await this.nutritionGoalsRepository.find();
     return nutritionGoals;
-}
+  }
 
+    async create(data: CreateNutritionGoalDto) {
+        const existingNutritionGoal = await this.nutritionGoalsRepository.findOne({
+            where: { name: data.name },
+        });
+        if (existingNutritionGoal) {
+            throw new ConflictException(
+                `Nutrition Goal with name ${data.name} already exists`,
+            );
+        }
+        const newNutritionGoal = this.nutritionGoalsRepository.create(data);
+        return this.nutritionGoalsRepository.save(newNutritionGoal);
+    }
+}
