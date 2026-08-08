1552f71bf18d2482013e41182118ef23d4864ea5
Andres <andresrojo20013@gmail.com>
2026-08-08 12:49:12 -0500

    se genero servicio de foods #54

--- src/foods/foods.module.ts
+++ src/foods/foods.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Foods } from './entities/Foods.entity';
+import { FoodsService } from './services/foods.service';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Foods])],
-    providers: [],
+    providers: [FoodsService],
     exports: [],
     controllers: [],
 })
